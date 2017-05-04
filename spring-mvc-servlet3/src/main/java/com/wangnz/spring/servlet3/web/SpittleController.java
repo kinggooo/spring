36228f8aa26c5adcc288f6606598427fcc804ae1
wangnz <5323700@qq.com>
2017-05-04 17:29:27 +0800

    add

--- spring-mvc-servlet3/src/main/java/com/wangnz/spring/servlet3/web/SpittleController.java
+++ spring-mvc-servlet3/src/main/java/com/wangnz/spring/servlet3/web/SpittleController.java
@@ -4,6 +4,7 @@ import com.wangnz.spring.servlet3.entity.Spittle;
 import com.wangnz.spring.servlet3.service.SpittleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,15 +25,21 @@ public class SpittleController {
 //        this.spittleService = spittleService;
 //    }
 
-    @RequestMapping(value = "test1", method = RequestMethod.GET)
+    @RequestMapping(value = "/test1", method = RequestMethod.GET)
     public String test1(Model model) {
         model.addAttribute("spittleList", createSpittleList(20));
         return "spittles";
     }
 
-    @RequestMapping(value = "test2", method = RequestMethod.GET)
+    @RequestMapping(value = "/test2", method = RequestMethod.GET)
     public String test2(@RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max) {
         System.out.println(max);
+        return "spittles";
+    }
+
+    @RequestMapping(value = "/test3/{spittleId}", method = RequestMethod.GET)
+    public String test3(@PathVariable(value = "spittleId") long spittleId) {
+        System.out.println(spittleId);
         return "spittles";
     }
 
