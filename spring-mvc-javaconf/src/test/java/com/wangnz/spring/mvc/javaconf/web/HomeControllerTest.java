522c53196c55785854b98ecc7e89f15ab21b9221
wangnz <5323700@qq.com>
2017-05-16 15:12:37 +0800

    add

--- spring-mvc-javaconf/src/test/java/com/wangnz/spring/mvc/javaconf/web/HomeControllerTest.java
+++ spring-mvc-javaconf/src/test/java/com/wangnz/spring/mvc/javaconf/web/HomeControllerTest.java
@@ -1,13 +1,12 @@
 package com.wangnz.spring.mvc.javaconf.web;
 
-import com.wangnz.spring.mvc.javaconf.web.HomeController;
 import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @Controller
 public class HomeControllerTest {
